920a0844e367ee1a0e37a31075bd9d01252708be
Owolabi Oyekanmi <ttconfirmed@gmail.com>
2022-08-15 13:16:56 +0100

    Delete src directory

--- src/routes/packages.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from "express";
-import { createPackage, getAllPackages } from '../controllers/package';
-
-const router = Router();
-
-router.post('/', createPackage);
-router.get('/', getAllPackages);
-export default router;
