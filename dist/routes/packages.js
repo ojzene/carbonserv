0eae7f270a4fbe73f3c94d9f3ca6d9ff41f9fa65
Owolabi Oyekanmi <ttconfirmed@gmail.com>
2022-08-15 13:16:39 +0100

    Delete dist directory

--- dist/routes/packages.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const package_1 = require("../controllers/package");
-const router = (0, express_1.Router)();
-router.post('/', package_1.createPackage);
-router.get('/', package_1.getAllPackages);
-exports.default = router;
